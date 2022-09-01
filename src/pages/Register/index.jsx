b17ab17fe9e95cea36477c01aa730a18899f59fc
Juliana <julianaha01@gmail.com>
2022-08-31 22:31:40 -0300

    feat: inicia paginas de cadastro

---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,52 @@
+const Register = () => {
+  <>
+    <h2>Inscreva-se na nossa escola</h2>
+    <p>
+      No momento não estamos com processo seletivo aberto, ok? Mas em breve
+      teremos novas turmas. Inscreva-se na lista de espera e fique sabendo em
+      primeira mão.
+    </p>
+    <form>
+      <div>
+        <label>
+            Nome:
+        </label>
+        <input/>
+        <label>
+            Sobrenomeome:
+        </label>
+        <input/>
+      </div>
+      <div>
+        <label>
+            Telefone:
+        </label>
+        <input/>
+        <label>
+            E-mail:
+        </label>
+        <input/>
+      </div>
+      <div>
+        <label>
+            CPF:
+        </label>
+        <input/>
+        <label>
+            Endereço:
+        </label>
+        <input/>
+      </div>
+      <div>
+        <label>
+            Curso:
+        </label>
+        <input/>
+        <label>
+            Unidade:
+        </label>
+        <input/>
+      </div>
+    </form>
+  </>;
+};
