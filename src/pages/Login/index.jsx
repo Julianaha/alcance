6ea5550252ad2d455f996ee78143763e2153a302
Juliana Souza <julianaha01@gmail.com>
2022-08-31 22:34:17 -0300

    feat: inicia paginas de cadastro (#1)

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,15 @@
+const Login = () => {
+    <>
+    <form>
+        <h1>Login</h1>
+        <p>Portal do aluno</p>
+        <div>
+            <label>E-mail:</label>
+            <input/>
+            <label>Senha:</label>
+            <input/>
+            <button type="submit">Entrar</button>
+        </div>
+    </form>
+    </>
+}
