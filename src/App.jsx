2b71013879ba750f75c53458c50ef6a8732be43f
Roberta <roberttavalentim07@icloud.com>
2022-09-02 23:38:41 -0300

    feat: componente navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import React from 'react';
+import Navbar from "./components/Header/Navbar";
 import "./styles/global.css";
+import { BrowserRouter as Router} from 'react-router-dom';
+
+import Container from './components/Header/Container';
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <Container customClass="min-height">
+    <Router>
+       <Navbar />
+    </Router>
+    </Container>
+
+  );
 }
 
 export default App;
