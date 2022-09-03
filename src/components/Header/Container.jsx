d8f5a50b743134b9ab30695b6d6ad28f69dd3a98
Roberta <roberttavalentim07@icloud.com>
2022-09-03 17:31:44 -0300

    feat: header com erro

--- src/components/Header/Container.jsx
+++ src/components/Header/Container.jsx
@@ -1,7 +1,7 @@
 import styles from './Container.module.css'
 
 function Container(props) {
-    return <div className={`${styles.container} ${styles[props.customClass]}`}>
+    return <div className={`${styles.container} ${styles.header} ${styles[props.customClass]}`}>
         {props.children}
         </div>
 }
