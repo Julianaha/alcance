cbf3cf29662dc9f152a1c91d03518982021bfa5c
Juliana Souza <julianaha01@gmail.com>
2022-09-03 16:51:54 -0300

    login (#5)
    * feat: inicia paginas de cadastro
    
    * feat: pagina de login

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import { Login } from "../../pages/Login";
+import Register from "../../components/Register";
+
+export const Home = () => {
+  return (
+    <>
+      <Login />
+      <Register />
+    </>
+  );
+};
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,15 +1,43 @@
-const Login = () => {
-    <>
-    <form>
-        <h1>Login</h1>
-        <p>Portal do aluno</p>
-        <div>
-            <label>E-mail:</label>
-            <input/>
-            <label>Senha:</label>
-            <input/>
-            <button type="submit">Entrar</button>
+import { FaAngleLeft } from "react-icons/fa";
+import login from "../../assets/login.png";
+import alcance from "../../assets/footerAlcance.png";
+import styles from "./Login.module.css";
+
+export const Login = () => {
+  return (
+    <div className={styles.grid}>
+      <main className={styles.main}>
+        <p className={styles.nav}>
+          <a>
+            <FaAngleLeft />
+            voltar para a home
+          </a>
+        </p>
+        <div className={styles.container}>
+          <form className={styles.form}>
+            <div className={styles.form_login}>
+              <img src={login} className={styles.logo} />
+              <div className={styles.user}>
+                <label htmlFor="mail">Usuário</label>
+                <input type="email" id="mail" name="usermail" />
+                <label htmlFor="password">Senha</label>
+                <input type="password" id="password" name="password" />
+                <button type="submit" className={styles.button}>
+                  entrar
+                </button>
+              </div>
+              <p className={styles.register}>
+                Não tem uma conta? <a href="">Cadastre-se</a>
+              </p>
+            </div>
+          </form>
         </div>
-    </form>
-    </>
-}
+      </main>
+      <footer className={styles.footer}>
+        <figure className={styles.img}>
+          <img src={alcance} />
+        </figure>
+      </footer>
+    </div>
+  );
+};
